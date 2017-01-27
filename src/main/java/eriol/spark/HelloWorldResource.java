e4ab8dbcf02d8af3727977639a4ad60b4b2ff1f7
Riol Fernandez <eriol-externo@cnmc.es>
2017-01-27 09:07:35 +0100

    Primer ejemplo básico

---
+++ src/main/java/eriol/spark/HelloWorldResource.java
@@ -0,0 +1,11 @@
+package eriol.spark;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class HelloWorldResource {
+	public static void main(String[] args) {
+		port(8088);
+		get("/helloworld", (req, res) -> "Hello World");
+	}	
+}
